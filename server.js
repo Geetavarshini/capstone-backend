d0fbb2cd555f36342d441c1186e9c9b5263022f9
Narala Geetavarshini <23eg105j43@anurag.edu.in>
2026-04-19 21:40:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app=exp()
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://capstone-frontend-r481tqa5z-geetavarshinis-projects.vercel.app"
+    "https://capstone-frontend-6fj6xfdlz-geetavarshinis-projects.vercel.app"
   ],
   credentials: true,
 }));
